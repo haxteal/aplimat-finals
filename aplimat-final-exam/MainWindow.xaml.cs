37c9b3feb3872aa153e0ec49ca24c8299143e1bd
Dale Julian <gg.dalejulian@gmail.com>
2018-04-07 01:15:40 +0800

    Aplimat Finals Template

---
+++ aplimat-final-exam/MainWindow.xaml.cs
@@ -0,0 +1,136 @@
+﻿using aplimat_final_exam.Models;
+using SharpGL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace aplimat_final_exam
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Vector3 mousePos = new Vector3();
+
+        #region Initialization
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenGLControl_OpenGLInitialized(object sender, SharpGL.SceneGraph.OpenGLEventArgs args)
+        {
+            OpenGL gl = args.OpenGL;
+
+            gl.Enable(OpenGL.GL_DEPTH_TEST);
+
+            float[] global_ambient = new float[] { 0.5f, 0.5f, 0.5f, 1.0f };
+            float[] light0pos = new float[] { 0.0f, 5.0f, 10.0f, 1.0f };
+            float[] light0ambient = new float[] { 0.2f, 0.2f, 0.2f, 1.0f };
+            float[] light0diffuse = new float[] { 0.3f, 0.3f, 0.3f, 1.0f };
+            float[] light0specular = new float[] { 0.8f, 0.8f, 0.8f, 1.0f };
+
+            float[] lmodel_ambient = new float[] { 0.2f, 0.2f, 0.2f, 1.0f };
+            gl.LightModel(OpenGL.GL_LIGHT_MODEL_AMBIENT, lmodel_ambient);
+
+            gl.LightModel(OpenGL.GL_LIGHT_MODEL_AMBIENT, global_ambient);
+            gl.Light(OpenGL.GL_LIGHT0, OpenGL.GL_POSITION, light0pos);
+            gl.Light(OpenGL.GL_LIGHT0, OpenGL.GL_AMBIENT, light0ambient);
+            gl.Light(OpenGL.GL_LIGHT0, OpenGL.GL_DIFFUSE, light0diffuse);
+            gl.Light(OpenGL.GL_LIGHT0, OpenGL.GL_SPECULAR, light0specular);
+            gl.Disable(OpenGL.GL_LIGHTING);
+            gl.Disable(OpenGL.GL_LIGHT0);
+
+            gl.ShadeModel(OpenGL.GL_SMOOTH);
+        }
+
+        #endregion
+
+        #region Mouse Func
+        private void OnMouseMove(object sender, MouseEventArgs e)
+        {
+            var position = e.GetPosition(this);
+            mousePos.x = (float)position.X - (float)Width / 2.0f;
+            mousePos.y = -((float)position.Y - (float)Height / 2.0f);
+        }
+        #endregion
+
+
+        private void ManageKeyPress()
+        {
+
+        }
+
+        private CubeMesh myCube = new CubeMesh();
+        private float speed = 0.1f;
+        private float bulletSpeed = 3.0f;
+
+        private CubeMesh bullet = new CubeMesh()
+        {
+            Position = new Vector3(100, 100, 0)
+        };
+
+        private void OpenGLControl_OpenGLDraw(object sender, SharpGL.SceneGraph.OpenGLEventArgs args)
+        {
+            this.Title = "APLIMAT Final Exam";
+            OpenGL gl = args.OpenGL;
+
+            // Clear The Screen And The Depth Buffer
+            gl.Clear(OpenGL.GL_COLOR_BUFFER_BIT | OpenGL.GL_DEPTH_BUFFER_BIT);
+
+            // Move Left And Into The Screen
+            gl.LoadIdentity();
+            gl.Translate(0.0f, 0.0f, -100.0f);
+
+            gl.Color(1.0, 0.0, 0.0);
+            myCube.Draw(gl);
+
+            gl.Color(1.0, 1.0, 1.0);
+            bullet.Draw(gl);
+
+            if (Keyboard.IsKeyDown(Key.W))
+            { 
+                myCube.ApplyForce(Vector3.Up * speed);
+            }
+
+            if (Keyboard.IsKeyDown(Key.D))
+            {
+                myCube.ApplyForce(Vector3.Right * speed);
+            }
+
+            if (Keyboard.IsKeyDown(Key.A))
+            {
+                myCube.ApplyForce(Vector3.Left * speed);
+            }
+            if (Keyboard.IsKeyDown(Key.S))
+            {
+                myCube.ApplyForce(Vector3.Down * speed);
+            }
+
+            if (Mouse.LeftButton == MouseButtonState.Pressed)
+            {
+
+            }
+
+            //myCube.ApplyGravity();
+
+            //if (myCube.Position.y <= -50)
+            //{
+            //    myCube.Position.y = -50;
+            //    myc
+            //}
+        }
+    }
+}
